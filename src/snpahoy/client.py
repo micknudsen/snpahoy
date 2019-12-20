a427b15d1fd7874f888f0f9d4ba39cd31988d487
Michael Knudsen <micknudsen@gmail.com>
2019-12-20 12:27:32 +0100

    Added ArgumentParser in client

--- src/snpahoy/client.py
+++ src/snpahoy/client.py
@@ -1,2 +1,12 @@
+import argparse
+
+
 def main() -> None:
-    print('Hello, World!')
+
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('--snp_bed', '-b', type=str, required=True)
+    parser.add_argument('--tumor_bam', '-t', type=str, required=True)
+    parser.add_argument('--normal_bam', '-n', type=str, required=True)
+
+    args = parser.parse_args()

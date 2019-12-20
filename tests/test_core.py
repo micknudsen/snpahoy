bad6904dda8e98b131808fee21469ee97f0d502d
Michael Knudsen <micknudsen@gmail.com>
2019-12-20 10:51:33 +0100

    Added tests for Genotyper class

--- tests/test_core.py
+++ tests/test_core.py
@@ -1,6 +1,8 @@
 import unittest
 
 from snpahoy.core import Position
+from snpahoy.core import Genotyper
+from snpahoy.core import GenotypeClass
 
 
 class TestPosition(unittest.TestCase):
@@ -13,3 +15,38 @@ class TestPosition(unittest.TestCase):
 
     def test_get_coordinate(self):
         self.assertEqual(self.position.coordinate, 10000)
+
+
+class TestGenotyper(unittest.TestCase):
+
+    def setUp(self):
+
+        positions = [Position(chromosome='chr1', coordinate=10000),
+                     Position(chromosome='chr1', coordinate=25000),
+                     Position(chromosome='chr2', coordinate=15000),
+                     Position(chromosome='chr3', coordinate=50000),
+                     Position(chromosome='chr4', coordinate=75000)]
+
+        self.genotyper = Genotyper(minimum_base_count=30,
+                                   homozygosity_threshold=0.95,
+                                   positions=positions)
+
+    def test_genotyper(self):
+
+        def get_base_counts(position):
+
+            counts = {('chr1', 10000): {'A': 50, 'C': 0, 'G': 0, 'T': 0},
+                      ('chr1', 25000): {'A': 0, 'C': 50, 'G': 50, 'T': 0},
+                      ('chr2', 15000): {'A': 0, 'C': 95, 'G': 5, 'T': 0},
+                      ('chr3', 50000): {'A': 0, 'C': 0, 'G': 90, 'T': 10},
+                      ('chr4', 75000): {'A': 20, 'C': 5, 'G': 0, 'T': 0}}
+
+            return counts((position.chromosome, position.coordinate))
+
+        expected_genotypes = [GenotypeClass.HOMOZYGOTE,
+                              GenotypeClass.HETEROZYGOTE,
+                              GenotypeClass.HOMOZYGOTE,
+                              GenotypeClass.HETEROZYGOTE,
+                              GenotypeClass.LOWCOVERAGE]
+
+        self.assertEqual(self.genotyper(get_base_counts), expected_genotypes)

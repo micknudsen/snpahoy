5a3a49166a99828171aa9487b43a92efa9ab6da1
Michael Knudsen <micknudsen@gmail.com>
2019-12-22 20:01:41 +0100

    Refactoring

--- src/snpahoy/core.py
+++ src/snpahoy/core.py
@@ -1,13 +1,17 @@
 from enum import Enum
-from typing import Tuple, NamedTuple
-
-
-Counts = Tuple[int, int, int, int]
+from typing import NamedTuple
 
 
 class Position(NamedTuple):
     chromosome: str
     coordinate: int
+
+
+class Counts(NamedTuple):
+    a: int
+    c: int
+    g: int
+    t: int
 
 
 class GenotypeClass(Enum):

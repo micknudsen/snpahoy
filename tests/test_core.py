d22ef02e26fb20f36be57ab9280054d79affd201
Michael Knudsen <micknudsen@gmail.com>
2019-12-23 06:16:42 +0100

    Test maf property at uncovered position

--- tests/test_core.py
+++ tests/test_core.py
@@ -9,13 +9,21 @@ from snpahoy.core import GenotypeClass
 
 class TestSNP(unittest.TestCase):
 
-    def setUp(self):
-        self.snp = SNP(position=Position(chromosome='chr1', coordinate=1000),
-                       counts=Counts(a=95, c=1, g=3, t=1),
-                       genotype=GenotypeClass.HOMOZYGOTE)
-
     def test_maf(self):
-        self.assertEqual(self.snp.maf, 0.03)
+
+        snp = SNP(position=Position(chromosome='chr1', coordinate=1000),
+                  counts=Counts(a=95, c=1, g=3, t=1),
+                  genotype=GenotypeClass.HOMOZYGOTE)
+
+        self.assertEqual(snp.maf, 0.03)
+
+    def test_maf_uncovered_position(self):
+
+        snp = SNP(position=Position(chromosome='chr1', coordinate=1000),
+                  counts=Counts(a=0, c=0, g=0, t=0),
+                  genotype=GenotypeClass.LOWCOVERAGE)
+
+        self.assertEqual(snp.maf, 0.0)
 
 
 class TestGenotyper(unittest.TestCase):

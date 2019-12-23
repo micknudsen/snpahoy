c4cd6b3780abaf1f1aa7de2d6476b7d3386f7685
Michael Knudsen <micknudsen@gmail.com>
2019-12-23 07:35:24 +0100

    Added test for get_snps function

--- tests/test_core.py
+++ tests/test_core.py
@@ -38,3 +38,20 @@ class TestCore(unittest.TestCase):
         self.assertEqual(genotyper.genotype(Counts(a=0, c=95, g=5, t=0)), GenotypeClass.HOMOZYGOTE)
         self.assertEqual(genotyper.genotype(Counts(a=0, c=0, g=90, t=10)), GenotypeClass.HETEROZYGOTE)
         self.assertEqual(genotyper.genotype(Counts(a=20, c=5, g=0, t=0)), GenotypeClass.LOWCOVERAGE)
+
+    def test_get_snps(self):
+
+        genotyper = Genotyper(minimum_coverage=30, homozygosity_threshold=0.95)
+
+        positions = [Position(chromosome='chr1', coordinate=1000),
+                     Position(chromosome='chr2', coordinate=5000)]
+
+        def get_counts(position: Position) -> Counts:
+            counts = {Position(chromosome='chr1', coordinate=1000): Counts(a=50, c=0, g=0, t=0),
+                      Position(chromosome='chr2', coordinate=5000): Counts(a=0, c=30, g=30, t=0)}
+            return counts[position]
+
+        snps = get_snps(postions=positions, genotyper=genotyper, get_counts=get_counts)
+
+        self.assertEqual(snps, [SNP(position=Position(chromosome='chr1', coordinate=1000), counts=Counts(a=50, c=0, g=0, t=0), genotype=GenotypeClass.HOMOZYGOTE),
+                                SNP(position=Position(chromosome='chr2', coordinate=5000), counts=Counts(a=0, c=30, g=30, t=0), genotype=GenotypeClass.HETEROZYGOTE)])

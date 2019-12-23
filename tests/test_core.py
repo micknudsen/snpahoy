c58dbe934d0153893ed4c0c0cb9ff85d2918d1fc
Michael Knudsen <micknudsen@gmail.com>
2019-12-23 06:38:18 +0100

    More explicit use of Counts class

--- tests/test_core.py
+++ tests/test_core.py
@@ -27,13 +27,14 @@ class TestSNP(unittest.TestCase):
         self.assertEqual(snp.maf, 0.0)
 
 
-class TestGenotyper(unittest.TestCase):
+class TestCore(unittest.TestCase):
 
     def test_genotyper(self):
+
         genotyper = Genotyper(minimum_coverage=30, homozygosity_threshold=0.95)
 
-        self.assertEqual(genotyper.genotype((50, 0, 0, 0)), GenotypeClass.HOMOZYGOTE)
-        self.assertEqual(genotyper.genotype((0, 50, 50, 0)), GenotypeClass.HETEROZYGOTE)
-        self.assertEqual(genotyper.genotype((0, 95, 5, 0)), GenotypeClass.HOMOZYGOTE)
-        self.assertEqual(genotyper.genotype((0, 0, 90, 10)), GenotypeClass.HETEROZYGOTE)
-        self.assertEqual(genotyper.genotype((20, 5, 0, 0)), GenotypeClass.LOWCOVERAGE)
+        self.assertEqual(genotyper.genotype(Counts(a=50, c=0, g=0, t=0)), GenotypeClass.HOMOZYGOTE)
+        self.assertEqual(genotyper.genotype(Counts(a=0, c=50, g=50, t=0)), GenotypeClass.HETEROZYGOTE)
+        self.assertEqual(genotyper.genotype(Counts(a=0, c=95, g=5, t=0)), GenotypeClass.HOMOZYGOTE)
+        self.assertEqual(genotyper.genotype(Counts(a=0, c=0, g=90, t=10)), GenotypeClass.HETEROZYGOTE)
+        self.assertEqual(genotyper.genotype(Counts(a=20, c=5, g=0, t=0)), GenotypeClass.LOWCOVERAGE)

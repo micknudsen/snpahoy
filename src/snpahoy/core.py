006080a30ea8f456f96ccf64ad033424935c8f5d
Michael Knudsen <micknudsen@gmail.com>
2019-12-23 10:48:48 +0100

    Less strict about instance variables

--- src/snpahoy/core.py
+++ src/snpahoy/core.py
@@ -24,35 +24,35 @@ class GenotypeClass(Enum):
 class SNP:
 
     def __init__(self, position: Position, counts: Counts, genotype: GenotypeClass = GenotypeClass.UNKNOWN):
-        self._position = position
-        self._counts = counts
-        self._genotype = genotype
+        self.position = position
+        self.counts = counts
+        self.genotype = genotype
 
     @property
     def coverage(self) -> int:
-        return sum(self._counts)
+        return sum(self.counts)
 
     @property
     def maf(self) -> float:
         if self.coverage == 0:
             return 0.0
-        return sorted(self._counts, reverse=True)[1] / self.coverage
+        return sorted(self.counts, reverse=True)[1] / self.coverage
 
 
 class Genotyper:
 
     def __init__(self, minimum_coverage: int, homozygosity_threshold: float) -> None:
-        self._minimum_coverage = minimum_coverage
-        self._homozygosity_threshold = homozygosity_threshold
+        self.minimum_coverage = minimum_coverage
+        self.homozygosity_threshold = homozygosity_threshold
 
     def genotype(self, counts: Counts) -> GenotypeClass:
 
         coverage = sum(counts)
-        if coverage == 0 or coverage < self._minimum_coverage:
+        if coverage == 0 or coverage < self.minimum_coverage:
             return GenotypeClass.LOWCOVERAGE
 
         frequencies = [count / coverage for count in counts]
-        if max(frequencies) < self._homozygosity_threshold:
+        if max(frequencies) < self.homozygosity_threshold:
             return GenotypeClass.HETEROZYGOTE
 
         return GenotypeClass.HOMOZYGOTE

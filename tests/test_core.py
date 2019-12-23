f71c88e30018f5160254ea5b6de962974bf88739
Michael Knudsen <micknudsen@gmail.com>
2019-12-23 06:34:17 +0100

    Removed blank lines here and there

--- tests/test_core.py
+++ tests/test_core.py
@@ -10,7 +10,6 @@ from snpahoy.core import GenotypeClass
 class TestSNP(unittest.TestCase):
 
     def setUp(self):
-
         self.snp = SNP(position=Position(chromosome='chr1', coordinate=1000),
                        counts=Counts(a=95, c=1, g=3, t=1),
                        genotype=GenotypeClass.HOMOZYGOTE)
@@ -22,18 +21,15 @@ class TestSNP(unittest.TestCase):
         self.assertEqual(self.snp.maf, 0.03)
 
     def test_maf_uncovered_position(self):
-
         snp = SNP(position=Position(chromosome='chr1', coordinate=1000),
                   counts=Counts(a=0, c=0, g=0, t=0),
                   genotype=GenotypeClass.LOWCOVERAGE)
-
         self.assertEqual(snp.maf, 0.0)
 
 
 class TestGenotyper(unittest.TestCase):
 
     def test_genotyper(self):
-
         genotyper = Genotyper(minimum_coverage=30, homozygosity_threshold=0.95)
 
         self.assertEqual(genotyper.genotype((50, 0, 0, 0)), GenotypeClass.HOMOZYGOTE)

65932d24c66592472849efae5a5ffea534937bbc
Michael Knudsen <micknudsen@gmail.com>
2019-12-24 16:50:13 +0100

    Added test for parse_bed_file function

--- tests/test_parsers.py
+++ tests/test_parsers.py
@@ -5,18 +5,18 @@ import unittest
 # from snpahoy.core import Genotyper
 
 # from snpahoy.parsers import get_snps
-from snpahoy.parsers import parse_bed_input
+from snpahoy.parsers import parse_bed_file
 
 
 class TestParsers(unittest.TestCase):
 
-    def test_parse_bed_input(self):
+    def test_parse_bed_file(self):
 
         bed_file_lines = ['\t'.join(['chr1', '1000', '1001']),
                           '\t'.join(['chr2', '5000', '5001']),
                           '\t'.join(['chr3', '2000', '2002'])]
 
-        self.assertEqual(parse_bed_input(stream=bed_file_lines), [('chr1', 1000), ('chr2', 5000), ('chr3', 2000), ('chr3', 2001)])
+        self.assertEqual(parse_bed_file(stream=bed_file_lines), [('chr1', 1000), ('chr2', 5000), ('chr3', 2000), ('chr3', 2001)])
 
 #     def test_get_snps(self):
 

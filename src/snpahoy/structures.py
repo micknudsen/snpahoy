53d6c7a921eb91c66ec778ea1f0ee52e9c9a30ff
Michael Knudsen <micknudsen@gmail.com>
2019-12-24 16:51:16 +0100

    Uncommented all structures

--- src/snpahoy/structures.py
+++ src/snpahoy/structures.py
@@ -1,17 +1,17 @@
-from typing import NamedTuple
-from typing import Tuple
+# from typing import NamedTuple
+# from typing import Tuple
 
 
-class Position(NamedTuple):
-    chromosome: str
-    coordinate: int
+# class Position(NamedTuple):
+#     chromosome: str
+#     coordinate: int
 
 
-class Counts(NamedTuple):
-    a: int
-    c: int
-    g: int
-    t: int
+# class Counts(NamedTuple):
+#     a: int
+#     c: int
+#     g: int
+#     t: int
 
 
-Genotype = Tuple[str, str]
+# Genotype = Tuple[str, str]

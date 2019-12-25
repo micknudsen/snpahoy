0d7c5270981219b7fc59d3da9fc177d22943d1e9
Michael Knudsen <micknudsen@gmail.com>
2019-12-25 22:09:38 +0100

    Write heterozygotes fractions to output JSON file

--- src/snpahoy/client.py
+++ src/snpahoy/client.py
@@ -2,6 +2,7 @@ import argparse
 import json
 import os
 
+from collections import defaultdict
 from statistics import mean
 from typing import Dict
 
@@ -32,13 +33,13 @@ def main():
     args = parser.parse_args()
 
     with open(args.bed_file, 'rt') as f:
-        coordinates = parse_bed_file(f.read().splitlines())
+        snp_coordinates = parse_bed_file(f.read().splitlines())
 
     genotyper = Genotyper(minimum_coverage=args.minimum_coverage,
                           homozygosity_threshold=args.homozygosity_threshold)
 
-    normal_snps = get_snps(coordinates=coordinates, genotyper=genotyper, get_counts=lambda chromosome, position: get_counts(alignment=AlignmentFile(args.normal_bam_file), chromosome=chromosome, position=position))
-    tumor_snps = get_snps(coordinates=coordinates, genotyper=genotyper, get_counts=lambda chromosome, position: get_counts(alignment=AlignmentFile(args.tumor_bam_file), chromosome=chromosome, position=position))
+    normal_snps = get_snps(coordinates=snp_coordinates, genotyper=genotyper, get_counts=lambda chromosome, position: get_counts(alignment=AlignmentFile(args.normal_bam_file), chromosome=chromosome, position=position))
+    tumor_snps = get_snps(coordinates=snp_coordinates, genotyper=genotyper, get_counts=lambda chromosome, position: get_counts(alignment=AlignmentFile(args.tumor_bam_file), chromosome=chromosome, position=position))
 
     # Only consider SNPs which are genotyped in both normal and tumor sample.
     genotyped_snp_pairs = []
@@ -54,20 +55,25 @@ def main():
         """Computes the mean minor allele frequency at sites which are homozygote in the NORMAL sample."""
         return mean([pair[sample].minor_allele_frequency() for pair in genotyped_snp_pairs if pair['normal'].is_homozygote()])
 
-    print(f'Number of genotyped SNPs ............. : {len(genotyped_snp_pairs)}')
-    print(f'Normal fraction of heterozygotes ..... : {count_heterozygotes(sample="normal") / len(genotyped_snp_pairs)}')
-    print(f'Tumor fraction of heterozygotes ...... : {count_heterozygotes(sample="tumor") / len(genotyped_snp_pairs)}')
     print(f'Normal mean minor allele frequency ... : {mean_minor_allele_frequency_at_homozygote_sites("normal")}')
     print(f'Tumor mean minor allele frequency .... : {mean_minor_allele_frequency_at_homozygote_sites("tumor")}')
 
     results = {}
 
-    results['input'] = {'bed-file': os.path.basename(args.bed_file),
-                        'normal-bam_file': os.path.basename(args.normal_bam_file),
-                        'tumor-bam-file': os.path.basename(args.tumor_bam_file)}
+    results['input'] = defaultdict(dict)
+    results['output'] = defaultdict(dict)
 
-    results['settings'] = {'minimum-coverage': args.minimum_coverage,
-                           'homozygosity-threshold': args.homozygosity_threshold}
+    results['input']['files'] = {'bed-file': os.path.basename(args.bed_file),
+                                 'normal-bam_file': os.path.basename(args.normal_bam_file),
+                                 'tumor-bam-file': os.path.basename(args.tumor_bam_file)}
+
+    results['input']['settings'] = {'minimum-coverage': args.minimum_coverage,
+                                    'homozygosity-threshold': args.homozygosity_threshold}
+
+    results['output']['summary'] = {'snps-total': len(snp_coordinates),
+                                    'snps-genotyped': len(genotyped_snp_pairs),
+                                    'heterozygotes-fraction-normal': float('%.2f' % (count_heterozygotes(sample='normal') / len(genotyped_snp_pairs))),
+                                    'heterozygotes-fraction-tumor': float('%.2f' % (count_heterozygotes(sample='tumor') / len(genotyped_snp_pairs)))}
 
     with open(args.output_json_file, 'w') as json_file_handle:
         json.dump(results, json_file_handle, indent=4)

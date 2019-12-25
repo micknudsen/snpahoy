73a3a4fccd97bb435c56171e5cbc676deabceb97
Michael Knudsen <micknudsen@gmail.com>
2019-12-25 16:28:27 +0100

    Removed a lot of ghosts from the past

--- src/snpahoy/client.py
+++ src/snpahoy/client.py
@@ -1,20 +1,18 @@
 import argparse
 
+from typing import Dict
+
 from pysam import AlignmentFile
 
-from snpahoy.core import Position
-from snpahoy.core import BaseCounts
 from snpahoy.core import Genotyper
 
 from snpahoy.parsers import get_snps
 from snpahoy.parsers import parse_bed_file
 
 
-def get_counts(alignment: AlignmentFile, position: Position) -> BaseCounts:
-    coverage = alignment.count_coverage(contig=position.chromosome,
-                                        start=position.coordinate,
-                                        stop=position.coordinate + 1)
-    return BaseCounts(A=coverage[0][0], C=coverage[1][0], G=coverage[2][0], T=coverage[3][0])
+def get_counts(alignment: AlignmentFile, chromosome: str, position: int) -> Dict[str, int]:
+    coverage = alignment.count_coverage(contig=chromosome, start=position, stop=position + 1)
+    return {'A': coverage[0][0], 'C': coverage[1][0], 'G': coverage[2][0], 'T': coverage[3][0]}
 
 
 def main():
@@ -30,16 +28,13 @@ def main():
     args = parser.parse_args()
 
     with open(args.bed_file, 'rt') as f:
-        positions = parse_bed_file(f.read().splitlines())
+        coordinates = parse_bed_file(f.read().splitlines())
 
     genotyper = Genotyper(minimum_coverage=args.minimum_coverage,
                           homozygosity_threshold=args.homozygosity_threshold)
 
-    normal_snps = get_snps(positions=positions, genotyper=genotyper, get_counts=lambda position: get_counts(alignment=AlignmentFile(args.normal_bam_file), position=position))
-    tumor_snps = get_snps(positions=positions, genotyper=genotyper, get_counts=lambda position: get_counts(alignment=AlignmentFile(args.tumor_bam_file), position=position))
+    normal_snps = get_snps(coordinates=coordinates, genotyper=genotyper, get_counts=lambda chromosome, position: get_counts(alignment=AlignmentFile(args.normal_bam_file), chromosome=chromosome, position=position))
+    tumor_snps = get_snps(coordinates=coordinates, genotyper=genotyper, get_counts=lambda chromosome, position: get_counts(alignment=AlignmentFile(args.tumor_bam_file), chromosome=chromosome, position=position))
 
-    selected_snp_pairs = []
-
-    for normal_snp, tumor_snp in zip(normal_snps, tumor_snps):
-        if normal_snp.is_genotyped() and tumor_snp.is_genotyped():
-            selected_snp_pairs.append([normal_snp, tumor_snp])
+    # Just testing...
+    print(len(normal_snps), len(tumor_snps))

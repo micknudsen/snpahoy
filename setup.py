fc39c487c47cd6431066c337f7edaaf3a0faef00
Michael Knudsen <micknudsen@gmail.com>
2020-06-08 14:45:15 +0200

    Bumped version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
 
     name='snpahoy',
-    version='0.3.2',
+    version='0.3.3',
 
     packages=find_packages('src'),
     package_dir={'': 'src'},

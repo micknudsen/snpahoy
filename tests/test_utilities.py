ad2f477d5ba5076e14b17e0f0a441000eb0670df
Michael Knudsen <michaelk@clin.au.dk>
2021-10-25 12:36:02 +0200

    Better test data for mean_off_genotype_frequency

--- tests/test_utilities.py
+++ tests/test_utilities.py
@@ -10,20 +10,20 @@ from snpahoy.utilities import mean_off_genotype_frequency
 class TestUtilities(unittest.TestCase):
 
     def setUp(self):
-        self.snps = [SNP(chromosome='chr1', position=1000, genotype='AA', counts={'A': 45, 'C': 1, 'G': 3, 'T': 1}),
-                     SNP(chromosome='chr2', position=5000, genotype='GT', counts={'A': 0, 'C': 0, 'G': 20, 'T': 30})]
+        self.snps = [SNP(chromosome='chr1', position=1000, genotype='AA', counts={'A': 46, 'C': 1, 'G': 3, 'T': 0}),
+                     SNP(chromosome='chr2', position=5000, genotype='GT', counts={'A': 2, 'C': 0, 'G': 18, 'T': 30})]
 
     def test_count_heterozygotes(self):
         self.assertEqual(count_heterozygotes(self.snps), 1)
 
     def test_mean_minor_allele_frequency(self):
-        self.assertEqual(mean_minor_allele_frequency(self.snps), 0.23)
+        self.assertEqual(mean_minor_allele_frequency(self.snps), 0.21)
 
     def test_mean_minor_allele_frequency_empty_list(self):
         self.assertEqual(mean_minor_allele_frequency([]), 0)
 
     def test_mean_off_genotype_frequency(self):
-        self.assertEqual(mean_off_genotype_frequency(self.snps), 0.05)
+        self.assertEqual(mean_off_genotype_frequency(self.snps), 0.06)
 
     def test_mean_off_genotype_frequency_empty_list(self):
         self.assertEqual(mean_off_genotype_frequency([]), 0)
